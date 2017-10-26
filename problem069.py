54d92118f91084fbbd4049cca09fd523fdaa666b
Roc-J <646481338@qq.com>
2017-10-26 17:28:46 +0800

    Sqrt()

---
+++ problem069.py
@@ -0,0 +1,11 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+import math
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        return int(math.sqrt(x))
