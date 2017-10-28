940027587630274fec83add1ba5f015f91e9eda2
Roc-J <646481338@qq.com>
2017-10-28 12:46:46 +0800

    Maximum subarray

--- problem053.py
+++ problem053.py
@@ -7,3 +7,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
+        if len(nums) == 0:
+            return 0
+        maxinum = nums[0]
+        result = 0
+        for i in range(len(nums)):
+            result += nums[i]
+            if maxinum < result:
+                maxinum = result
+            if result < 0:
+                result = 0
+
+        return maxinum
+
+if __name__ == '__main__':
+    print Solution().maxSubArray([-2, -2, -2])
