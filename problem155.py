f4709a06e4818bbe325b5c05b34756df4dfd24ea
Roc-J <646481338@qq.com>
2017-11-03 11:16:12 +0800

    Min stack

---
+++ problem155.py
@@ -0,0 +1,52 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+class MinStack(object):
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        self.stack.append(x)
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[len(self.stack)-1]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return min(self.stack)
+
+
+        # Your MinStack object will be instantiated and called as such:
+        # obj = MinStack()
+        # obj.push(x)
+        # obj.pop()
+        # param_3 = obj.top()
+        # param_4 = obj.getMin()
+
+if __name__ == '__main__':
+    obj = MinStack()
+    obj.push(-2)
+    obj.push(0)
+    obj.push(-3)
+    print obj.getMin()
+    obj.pop()
+    print obj.top()
+    print obj.getMin()
