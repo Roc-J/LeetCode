967fdb04addbe587c79356116296e4ea2c04d631
Roc-J <646481338@qq.com>
2017-11-13 00:00:34 +0800

    Reverse String

---
+++ problem344.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return s[::-1]
