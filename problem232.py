1c8b00796ec0c021913d1c37f96d131ea94a3d9e
Roc-J <646481338@qq.com>
2017-11-12 15:10:15 +0800

    Implement queue using stack

---
+++ problem232.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+class MyQueue(object):
+    def __init__(self):
+        """
+        Initialize your data structure here.
+        """
+        self.queue = []
+
+    def push(self, x):
+        """
+        Push element x to the back of queue.
+        :type x: int
+        :rtype: void
+        """
+        self.queue.append(x)
+
+    def pop(self):
+        """
+        Removes the element from in front of queue and returns that element.
+        :rtype: int
+        """
+        return self.queue.pop(0)
+
+    def peek(self):
+        """
+        Get the front element.
+        :rtype: int
+        """
+        return self.queue[0]
+
+    def empty(self):
+        """
+        Returns whether the queue is empty.
+        :rtype: bool
+        """
+        if len(self.queue) > 0:
+            return False
+        else:
+            return True
+
+
+
+        # Your MyQueue object will be instantiated and called as such:
+        # obj = MyQueue()
+        # obj.push(x)
+        # param_2 = obj.pop()
+        # param_3 = obj.peek()
+        # param_4 = obj.empty()
