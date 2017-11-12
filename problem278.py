259617bd6f9d188a78cfe2cd824fc73baf074e7d
Roc-J <646481338@qq.com>
2017-11-12 22:41:28 +0800

    First Bad Version

---
+++ problem278.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        left, right = 1, n
+        while left <= right:
+            mid = (left + right)/2
+            if isBadVersion(mid):
+                right = mid - 1
+            else:
+                left = mid + 1
+        return left
