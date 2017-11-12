164fda41b587a24043d40063ee6fa797bd1612ca
Roc-J <646481338@qq.com>
2017-11-12 23:33:15 +0800

    Nim game

---
+++ problem292.py
@@ -0,0 +1,10 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+class Solution(object):
+    def canWinNim(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        return n % 4 != 0
