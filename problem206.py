16c45f632f44762a2249676eec45ba7f6e67f77f
Roc-J <646481338@qq.com>
2017-11-12 14:22:34 +0800

    Reverse Linked List

---
+++ problem206.py
@@ -0,0 +1,28 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        cur_node = head
+        prev_node = None
+        next_node = None
+        while cur_node:
+            next_node = cur_node.next
+            cur_node.next = prev_node
+            prev_node = cur_node
+            cur_node = next_node
+        return prev_node
+
+
+
