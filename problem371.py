38633b23e5924ab8496970d25df2b624098f6009
Roc-J <646481338@qq.com>
2017-11-15 11:01:08 +0800

    Sum of Two Integers, 这个题用位运算，但是python的位运算有点麻烦

---
+++ problem371.py
@@ -0,0 +1,16 @@
+# -*- coding:utf-8 -*- 
+# Author: Roc-J
+
+class Solution(object):
+    def getSum(self, a, b):
+        """
+        :type a: int
+        :type b: int
+        :rtype: int
+        """
+        MAX_INT = 0x7FFFFFFF
+        MASK = 0x100000000
+        while b:
+            a, b = (a ^ b) % MASK, ((a & b) << 1) % MASK
+        return a if a <= MAX_INT else ~((a & MAX_INT) ^ MAX_INT)
+
