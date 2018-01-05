ee4dc2a712ad817ff455ceb28b47c3fe53afe67e
Roc-J <646481338@qq.com>
2018-01-05 16:22:29 +0800

    Find Minimun in Rotated Sorted Array 2

--- problem154.py
+++ problem154.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return min(nums)
