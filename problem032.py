1d312feedd54eb3d79e272e7d99342446d7a1e8d
Roc-J <646481338@qq.com>
2018-01-08 18:14:33 +0800

    Longest Valid Parentheses

--- problem032.py
+++ problem032.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution(object):
+    def longestValidParentheses(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        if len(s) <= 1:
+            return 0
+        stack = 0
+        max_ = [0]*len(s)
+        maxlen = 0
+        for i in range(len(s)):
+            if s[i] == '(':
+                stack += 1
+            elif s[i] == ')':
+                if stack > 0:
+                    stack -= 1
+                    max_[i] = max_[i-1] + 2
+                    if i - max_[i] >= 0 :
+                        max_[i] += max_[i - max_[i]]
+            maxlen = max(max_[i], maxlen)
+        return maxlen
+
+
+if __name__ == '__main__':
+    print(Solution().longestValidParentheses("()(())"))
+
