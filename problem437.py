ec642ddea90792bf6940c7517dd51d39c7a999e4
Roc-J <646481338@qq.com>
2018-01-08 21:23:52 +0800

    Path SUM III

---
+++ problem437.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*- 
+# Author: qjk
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    '''
+    使用递归
+
+    '''
+    def find_paths(self, root, target):
+        if root:
+            return int(root.val == target) + self.find_paths(root.left, target-root.val) + self.find_paths(root.right, target-root.val)
+        return 0
+
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: int
+        """
+        if root:
+            return self.find_paths(root, sum) + self.pathSum(root.left, sum) + self.pathSum(root.right, sum)
+        return 0
+
