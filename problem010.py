52125e33f58bcd5ede58f586fc49c7237114e6df
Roc-J <646481338@qq.com>
2018-02-25 11:59:28 +0800

    DP

--- problem010.py
+++ problem010.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*- 
 # Author: qjk
+
+class Solution(object):
+    def isMatch(self, s, p):
+        """
+        :type s: str
+        :type p: str
+        :rtype: bool
+        """
+        if not p:
+            return not s
+
+        first_match = bool(s) and p[0] in {s[0], '.'}
+
+        if len(p) >= 2 and p[1] == '*':
+            return self.isMatch(s, p[2:]) or first_match and self.isMatch(s[1:], p)
+        else:
+            return first_match and self.isMatch(s[1:], p[1:])
+
+if __name__ == '__main__':
+    print(Solution().isMatch("aaa", "a*a"))
